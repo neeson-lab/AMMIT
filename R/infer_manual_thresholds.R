ed13924652e6997e13306a6a752fbd0fbee1b3a3
Neeson Lab <patrick.crock@petermac.org>
2025-01-30 14:51:56 +1100

    Added README Updated infer_manual_thresholds Initialised manual

--- R/infer_manual_thresholds.R
+++ R/infer_manual_thresholds.R
@@ -1,6 +1,15 @@
+#' Title
+#'
+#' @param spe A SpatialExperiment object with assays 'data' and 'reference' present.
+#' @param markers A character vector of markers matching, at least partially, to the rownames of your SpatialExperiment object.
+#'
+#' @return The input SpatialExperiment object, with manual thresholds for each of the elements in markers added into the rowData under manual_thresholds
+#' @export
+#'
+#' @examples
 infer_manual_thresholds <- function(spe, markers) {
 
-  if (class(spe)!="SpatialExperiment") { stop("Your `spe` object is not a SpatialExperiment object!") }
+  if (is(spe, "SpatialExperiment")) { stop("Your `spe` object is not a SpatialExperiment object!") }
   if (!"reference" %in% names(assays(spe))) { stop("No reference data found in your SpatialExperiment object. Have you added reference data?") }
   if (!"data" %in% names(assays(spe))) { stop("No intensity data found in your SpatialExperiment object. Have you added intensity data?") }
   if (!any(markers %in% rownames(spe))) { stop("None of the specified markers were found in your SpatialExperiment object!")}
---
+++ README.md
@@ -0,0 +1,23 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# AMMIT
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+AMMIT is designed to streamline the processing of many mIHC samples, by
+applying a skew-normal mixture model trained on a small subset of
+samples.
+
+## Installation
+
+You can install the development version of AMMIT from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("pak")
+pak::pak("neeson-lab/AMMIT")
+```
+
+## Example
